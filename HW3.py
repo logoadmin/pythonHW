e3da3822801697b32760be0a0ce6f51eefe7c860
logoadmin <37313459+logoadmin@users.noreply.github.com>
2018-03-16 21:16:39 +0800

    HW3
    HW3

---
+++ HW3.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 16 20:33:10 2018
+
+@author: MSIZ77
+"""
+
+s=input("in:")
+T=list(s)
+a=[None]*len(T)
+for i in range (1,len(T)-1):
+    a[len(T)-1-i]=T[i]
+a[0]=T[0]
+a[len(T)-1]=T[len(T)-1]
+for i in range (0,len(a)-1):
+    if(a[i]==" "):
+        del a[i]
+    
+str="".join(a)
+print(str)
+    
