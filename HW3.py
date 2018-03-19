532fe3c5b516e0bde82ced375efbd5be0a438895
logoadmin <37313459+logoadmin@users.noreply.github.com>
2018-03-19 21:59:55 +0800

    HW3 V1.0

--- HW3.py
+++ HW3.py
@@ -5,17 +5,20 @@ Created on Fri Mar 16 20:33:10 2018
 @author: MSIZ77
 """
 
-s=input("in:")
-T=list(s)
-a=[None]*len(T)
-for i in range (1,len(T)-1):
-    a[len(T)-1-i]=T[i]
-a[0]=T[0]
-a[len(T)-1]=T[len(T)-1]
-for i in range (0,len(a)-1):
-    if(a[i]==" "):
-        del a[i]
+s=input("in:").split()
+x=""
+for j in range (0,len(s)):
+    T=list(s[j])
+    a=[None]*len(T)
+    for i in range (1,len(T)-1):
+        a[len(T)-1-i]=T[i]
+    a[0]=T[0]
+    a[len(T)-1]=T[len(T)-1]
+    #for i in range (0,len(a)-1):
+    #    if(a[i]==" "):
+    #        del a[i]
+    str="".join(a)
+    x=x+str
     
-str="".join(a)
-print(str)
+print(x)
     
